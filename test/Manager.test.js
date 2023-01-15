157fe082a6f3b1dc83384929f4b56e7820ae0eef
J0J0 <iditaroddream@gmail.com>
2023-01-15 11:26:27 -0600

    added manager test

---
+++ test/Manager.test.js
@@ -0,0 +1,21 @@
+const Manager = require("../lib/Manager");
+
+describe("Manager", () => {
+  describe("Initialization", () => {
+    it("should return an object containing a 'officeNumber' property when called with the 'new' keyword", () => {
+      const obj = new Manager();
+
+      expect("officeNumber" in obj).toEqual(true);
+    });
+    it("'officeNumber' should be a number", () => {
+      const obj = new Manager(1, "John Doe", "JD@gmail.com", 201);
+
+      expect(obj.officeNumber).toEqual(201);
+    });
+    it("getOfficeNumber method should return a number", () => {
+      const obj = new Manager(1, "John Doe", "JD@gmail.com", 201);
+
+      expect(obj.getOfficeNumber()).toEqual(201);
+    });
+})
+})
